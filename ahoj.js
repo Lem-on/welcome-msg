37293163cafaa757f52ed27dd604a61a6237ec1b
Lem-on <petr.smoldas@centrum.cz>
2016-03-06 19:44:45 +0100

    Update ahoj.js

--- ahoj.js
+++ ahoj.js
@@ -2,5 +2,6 @@ API.chatLog(/me :white_check_mark: WELCOME BOT RUN );
 
 API.on(API.USER_JOIN, function(data){
   if(message == ""){API.sendChat("@" + data.username Vítám tě! :)");
-} else {API.sendChat(message + " @" + data.username);}
+ } else {API.sendChat(message + " @" + data.username);
+}
 
